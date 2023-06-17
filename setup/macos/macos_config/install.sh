54219c6f20b7b0ccd49a9468c35b0c462d218707
Kento Monthubert <k.monthubert@gmail.com>
2023-06-17 19:13:30 +0200

    add macos config

---
+++ setup/macos/macos_config/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Mouse behaviour
+defaults write .GlobalPreferences com.apple.mouse.scaling -1
+
+# App behaviour
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write me.lukehaas.runjs ApplePressAndHoldEnabled -bool false
+
+# Typing behaviour
+defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 10
+
+echo "ℹ️ - You will need to restart the computer in order to apply changes."
+
