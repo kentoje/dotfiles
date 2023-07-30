a599cdf305508f3433fde929eb8910745bf36810
Kento Monthubert <k.monthubert@gmail.com>
2023-07-30 17:33:04 +0200

    update symlink script

--- setup/macos/setup_symlinks.sh
+++ setup/macos/setup_symlinks.sh
@@ -4,15 +4,22 @@ PWD=$(pwd)
 # .../dotfiles
 CLEANED_PATH="$(echo "$PWD" | sed 's/\/setup\/macos//')"
 
-if [ ! -e "$HOME/.config/starship.toml" ]; then
-	ln -s $CLEANED_PATH/.config/starship.toml $HOME/.config/starship.toml
+if [ -f "$HOME/.config/starship.toml" ]; then
+	echo "st"
+	rm "$HOME/.config/starship.toml"
 fi
 
-if [ ! -e "$HOME/.config/tmux/gitmux.conf" ]; then
-	ln -s $CLEANED_PATH/tmux/gitmux.conf $HOME/.config/tmux/gitmux.conf
+if [ -f "$HOME/.config/tmux/gitmux.conf" ]; then
+	echo "gitm"
+	rm "$HOME/.config/tmux/gitmux.conf"
 fi
 
-if [ ! -e "$HOME/.config/tmux/tmux.conf" ]; then
-	ln -s $CLEANED_PATH/tmux/tmux.conf $HOME/.config/tmux/tmux.conf
+if [ -f "$HOME/.config/tmux/tmux.conf" ]; then
+	echo "tmux"
+	rm "$HOME/.config/tmux/tmux.conf"
 fi
+
+ln -s $CLEANED_PATH/starship.toml $HOME/.config/starship.toml
+ln -s $CLEANED_PATH/tmux/gitmux.conf $HOME/.config/tmux/gitmux.conf
+ln -s $CLEANED_PATH/tmux/tmux.conf $HOME/.config/tmux/tmux.conf
 
