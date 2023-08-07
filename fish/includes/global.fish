05bf6d565c6dff243937dedc16f71b156c507449
Kento Monthubert <k.monthubert@gmail.com>
2023-08-07 16:09:39 +0200

    update fish

--- fish/includes/global.fish
+++ fish/includes/global.fish
@@ -1,6 +1,5 @@
 set -U fish_greeting
 set -U fish_key_bindings fish_vi_key_bindings
-set -U fish_color_autosuggestion a5adce 
+set -U fish_color_autosuggestion a5adce
 set -Ux EDITOR nvim
 set -Ux BAT_THEME Catppuccin-mocha
-
