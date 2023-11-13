7b187c1dbd755a3ea439b3a6470242fcf0b02307
Kento Monthubert <k.monthubert@gmail.com>
2023-11-13 16:45:35 +0100

    update conf

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,10 +1,7 @@
 return {
 	"numToStr/Comment.nvim",
 	config = function()
-		require("ts_context_commentstring").setup({})
-		require("Comment").setup({
-			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-		})
+		require("Comment").setup()
 
 		local ft = require("Comment.ft")
 		local api = require("Comment.api")
--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,7 +1,12 @@
 return {
 	"echasnovski/mini.nvim",
 	config = function()
-		require("mini.files").setup()
-		vim.keymap.set("n", "<leader>-", ":lua MiniFiles.open()<CR>", { desc = "Open the mini files tree" })
+		local minifiles = require("mini.files")
+
+		minifiles.setup()
+		vim.keymap.set("n", "<leader>-", function()
+			-- minifiles.open(vim.api.nvim_buf_get_name(0), false) -- cwd with new state
+			minifiles.open(vim.api.nvim_buf_get_name(0)) -- cwd but same state
+		end, { noremap = true })
 	end,
 }
