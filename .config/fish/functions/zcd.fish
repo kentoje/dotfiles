79f8d3f1d46b63d2d9fdf294f039d3ba343e88c4
Kento Monthubert <k.monthubert@gmail.com>
2025-01-07 16:29:58 +0100

    update fish

--- .config/fish/functions/zcd.fish
+++ .config/fish/functions/zcd.fish
@@ -1,5 +1,10 @@
 function zcd
-    set -l result (zoxide query --list | fzf-tmux -p)
+    if test -n "$argv"
+        set -l result (zoxide query --list | fzf-tmux -p --query "$argv")
+    else
+        set -l result (zoxide query --list | fzf-tmux -p)
+    end
+
     if test -n "$result"
         cd $result
     end
