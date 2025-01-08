ccd559c169454b5fbfcd2ab0f196a494686f4bb5
Kento Monthubert <k.monthubert@gmail.com>
2025-01-08 18:10:55 +0100

    fix zcd

--- .config/fish/functions/zcd.fish
+++ .config/fish/functions/zcd.fish
@@ -1,11 +1,11 @@
 function zcd
     if test -n "$argv"
-        set -l result (zoxide query --list | fzf-tmux -p --query "$argv")
+        set result (zoxide query --list | fzf-tmux -p --query "$argv")
     else
-        set -l result (zoxide query --list | fzf-tmux -p)
+        set result (zoxide query --list | fzf-tmux -p)
     end
 
     if test -n "$result"
-        cd $result
+        z $result
     end
 end
