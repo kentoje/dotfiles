ebb3236ba885f3373892b95c6b030b7ded358479
Kento Monthubert <k.monthubert@gmail.com>
2025-01-17 21:03:19 +0100

    add proton pass shortcut

--- .hammerspoon/open/init.lua
+++ .hammerspoon/open/init.lua
@@ -4,20 +4,6 @@ local function bind_app(key, app_path)
 	end)
 end
 
--- local toggleFocus = function(appName)
--- 	local app = hs.application.get(appName)
---
--- 	if app then
--- 		if app:isFrontmost() then
--- 			app:hide()
--- 		else
--- 			app:activate()
--- 		end
--- 	else
--- 		hs.application.launchOrFocus(appName)
--- 	end
--- end
-
 hs.hotkey.bind({ "alt" }, "e", function()
 	hs.execute("open -a Finder")
 end)
@@ -32,20 +18,4 @@ bind_app("z", "/Applications/zoom.us.app")
 bind_app("d", "/Applications/Tayasui Sketches.app")
 bind_app("w", "/Applications/WhatsApp.app")
 bind_app("b", "/Applications/Spark.app")
-
--- for _, item in ipairs({
--- 	{ "t", "Ghostty" },
--- 	{ "g", "Arc.app" },
--- 	{ "m", "Spotify" },
--- 	{ "s", "Slack" },
--- 	{ "f", "Figma" },
--- 	{ "c", "Rise" },
--- 	{ "z", "zoom.us" },
--- 	{ "d", "Tayasui Sketches" },
--- 	{ "w", "WhatsApp" },
--- 	{ "b", "Spark" },
--- }) do
--- 	hs.hotkey.bind({ "alt" }, item[1], function()
--- 		toggleFocus(item[2])
--- 	end)
--- end
+bind_app("p", "/Applications/Proton Pass.app")
