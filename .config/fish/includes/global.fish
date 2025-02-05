c1845284d409467f37e45cca03fcec8c1f075b8e
Kento Monthubert <k.monthubert@gmail.com>
2025-02-05 19:20:54 +0100

    set a bat config

--- .config/fish/includes/global.fish
+++ .config/fish/includes/global.fish
@@ -3,7 +3,6 @@ set -U fish_key_bindings fish_vi_key_bindings
 set -U fish_color_autosuggestion a5adce
 set -gx EDITOR nvim
 set -Ux VISUAL nvim
-set -Ux BAT_THEME Catppuccin-mocha
 set -Ux HOMEBREW_NO_AUTO_UPDATE 1
 set -Ux NIX_CONF_DIR $HOME/dotfiles/.config/nix-darwin
 
