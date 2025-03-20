6b9351da6b2d6b1ccddbaa3c27e7f362a1ff8963
Kento Monthubert <k.monthubert@gmail.com>
2025-03-20 10:03:15 +0100

    update fzf fish

--- .config/fish/includes/fzf.fish
+++ .config/fish/includes/fzf.fish
@@ -5,6 +5,7 @@ set -Ux FZF_DEFAULT_OPTS "\
 --pointer='' \
 --marker=' ' \
 --ansi \
+--bind 'alt-a:toggle-all' \
 --color='16,bg+:-1,gutter:-1,prompt:5,pointer:5,marker:6,border:4,label:4,header:italic'"
 set -Ux FZF_CTRL_R_OPTS "--border-label=' history ' \
 --prompt='  '"
