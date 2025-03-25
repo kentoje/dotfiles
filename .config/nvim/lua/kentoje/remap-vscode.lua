4b1b11b28711d57cb106f34d229d9d4e86796339
Kento Monthubert <k.monthubert@gmail.com>
2025-03-25 18:49:45 +0100

    add cursor custom config

--- .config/nvim/lua/kentoje/remap-vscode.lua
+++ .config/nvim/lua/kentoje/remap-vscode.lua
@@ -1,3 +1,7 @@
+local vscode = require("vscode")
+
+vim.g.mapleader = " "
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -13,3 +17,71 @@ vim.keymap.set("x", "p", '"_dP')
 -- vim.keymap.set("n", "<leader>y", '"+y')
 -- vim.keymap.set("v", "<leader>y", '"+y')
 -- vim.keymap.set("n", "<leader>Y", '"+Y')
+
+-- vim.keymap.set({ "n" }, "<C-l>", function()
+-- 	vscode.action("workbench.action.nextEditor")
+-- end)
+--
+-- vim.keymap.set({ "n" }, "<C-h>", function()
+-- 	vscode.action("workbench.action.previousEditor")
+-- end)
+
+vim.keymap.set({ "n" }, "<leader>l", function()
+	vscode.action("workbench.action.moveEditorToNextGroup")
+end)
+
+vim.keymap.set({ "n" }, "<leader>h", function()
+	vscode.action("workbench.action.moveEditorToPreviousGroup")
+end)
+
+vim.keymap.set({ "n" }, "<leader>d", function()
+	vscode.action("workbench.action.splitEditor")
+end)
+
+vim.keymap.set({ "n" }, "gd", function()
+	vscode.action("editor.action.showHover")
+end)
+
+vim.keymap.set({ "n" }, "gf", function()
+	vscode.action("editor.action.revealDefinition")
+end)
+
+vim.keymap.set({ "n" }, "gF", function()
+	vscode.action("editor.action.referenceSearch.trigger")
+end)
+
+vim.keymap.set({ "n" }, "gp", function()
+	vscode.action("workbench.action.quickOpen")
+end)
+
+vim.keymap.set({ "n" }, "<leader>fg", function()
+	vscode.action("workbench.action.findInFiles")
+end)
+
+vim.keymap.set({ "n" }, "<leader>fr", function()
+	vscode.action("search.action.focusSearchList")
+end)
+
+vim.keymap.set({ "n" }, "<leader>cr", function()
+	vscode.action("editor.action.rename")
+end)
+
+vim.keymap.set({ "n" }, "<leader>y", function()
+	vscode.action("copyFilePath")
+end)
+
+vim.keymap.set({ "n" }, "]e", function()
+	vscode.action("editor.action.marker.next")
+end)
+
+vim.keymap.set({ "n" }, "[e", function()
+	vscode.action("editor.action.marker.prev")
+end)
+
+vim.keymap.set({ "n", "x" }, "]]", function()
+	vscode.action("editor.action.nextSelectionMatchFindAction")
+end)
+
+vim.keymap.set({ "n", "x" }, "[[", function()
+	vscode.action("editor.action.previousSelectionMatchFindAction")
+end)
