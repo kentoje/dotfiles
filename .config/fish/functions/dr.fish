f250a6050dba4216788806c298452e760cf5eaa1
Kento Monthubert <k.monthubert@gmail.com>
2025-06-22 17:26:49 +0200

    update dr alias

--- .config/fish/functions/dr.fish
+++ .config/fish/functions/dr.fish
@@ -1,6 +1,6 @@
 function dr
     if string match -q "/Volumes*" $HOME
-        darwin-rebuild switch --flake ~/dotfiles/.config/nix-darwin-mini/#kento
+       sudo darwin-rebuild switch --flake ~/dotfiles/.config/nix-darwin-mini/#kento
     else
         darwin-rebuild switch --flake ~/dotfiles/.config/nix-darwin-pro/#kento
     end
