f6ae0b543c021db91236074d295881a2cef57363
Kento Monthubert <k.monthubert@gmail.com>
2026-02-24 15:08:15 +0100

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -30,6 +30,7 @@ fish_add_path ~/.config/bin
 fish_add_path ~/.config/fish/functions
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
+fish_add_path ~/.bun/bin
 
 fish_add_path /opt/homebrew/bin
 fish_add_path ~/.local/share/nvim/mason/bin
@@ -37,25 +38,21 @@ fish_add_path ~/.local/share/nvim/mason/bin
 
 set path (dirname (realpath ~/.config/fish/config.fish))
 
-source $path/includes/global.fish
-source $path/includes/fzf.fish
-source $path/includes/abbr.fish
-source $path/includes/alias.fish
-# source $path/includes/yz.fish
-
-if test -f $path/includes/private-vars.fish
-  source $path/includes/private-vars.fish
-end
+source ~/dotfiles/.config/fish/includes/global.fish
+source ~/dotfiles/.config/fish/includes/fzf.fish
+source ~/dotfiles/.config/fish/includes/abbr.fish
+source ~/dotfiles/.config/fish/includes/alias.fish
+source ~/dotfiles/.config/fish/includes/private-vars.fish
 
 # pnpm
 if string match -q "/Volumes*" $HOME
-  set -gx PNPM_HOME "/Volumes/HomeX/kento/Library/pnpm"
+    set -gx PNPM_HOME /Volumes/HomeX/kento/Library/pnpm
 else
-  set -gx PNPM_HOME "/Users/kento/Library/pnpm"
+    set -gx PNPM_HOME /Users/kento/Library/pnpm
 end
 
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
 
@@ -64,8 +61,7 @@ fish_add_path /Volumes/HomeX/kento/.codeium/windsurf/bin
 
 bind -M insert ctrl-space accept-autosuggestion
 
-alias cl="~/.local/bin/claude"
-
+alias cl="~/.local/bin/claude --dangerously-skip-permissions"
 
 # opencode
 fish_add_path /Volumes/HomeX/kento/.opencode/bin
